562df4cd0a8a7442aded5cc36e0fbda9dd4918e8
Yusuf Güngör <yusufgngor@gmail.com>
2021-07-08 00:04:46 +0300

    First Test Succeded

---
+++ src/Dollar.java
@@ -0,0 +1,11 @@
+public class Dollar {
+
+    int amount=10;
+    Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+    }
+
+}
---
+++ test/CurrencyTests.java
@@ -0,0 +1,17 @@
+
+public class CurrencyTests {
+    
+
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+
+
+
+    }
+
+
+
+
+}
